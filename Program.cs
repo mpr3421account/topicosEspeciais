d23c1f36ef9cbb8a0967fc2a48b77bb6f349a4f6
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-12 08:47:29 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//inferência de tipo
+//var reconhece o tipo de dado da variável e o compilador reconhece
+var x = 10;//inteiro
+var y = 20.0;//double
+var z = "Maria";//string
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
