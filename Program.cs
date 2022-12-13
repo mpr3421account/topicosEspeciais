008fc391740b3b7a5d6848dd9c1058563fb8461e
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-13 09:43:12 -0300

    Switch case - 1º com if / else

--- Program.cs
+++ Program.cs
@@ -7,3 +7,42 @@ var z = "Maria";//string
 Console.WriteLine(x);
 Console.WriteLine(y);
 Console.WriteLine(z);
+
+//switch-case
+//possibilidade de if's
+
+int num = int.Parse(Console.ReadLine());
+string day;
+
+if(num == 1)
+{
+    Console.WriteLine("Sunday");
+}
+else if(num == 2)
+{
+    Console.WriteLine("Monday");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Tuesday");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (num == 5)
+{
+    Console.WriteLine("Thursday");
+}
+else if (num == 6)
+{
+    Console.WriteLine("Friday");
+}
+else if (num == 7)
+{
+    Console.WriteLine("Saturday");
+}
+else
+{
+    Console.WriteLine("Invalid Value");
+}
